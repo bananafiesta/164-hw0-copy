9ab5eb6880b5079a11c436fd23838f7bc590c195
Class Account <cs164-aes@oxford>
2022-09-01 03:15:53 -0700

    finished hw0

--- lib/hw0.ml
+++ lib/hw0.ml
@@ -13,8 +13,8 @@ let close : float -> float -> bool =
 (** [square x] is the square of the number [x]. *)
 let square : float -> float =
   fun x ->
-    failwith "TODO"
-
+    (* failwith "TODO" *)
+    x *. x
 (******************************************************************************)
 (* Task 2 *)
 
@@ -24,15 +24,19 @@ let square : float -> float =
     produced by a step of the Babylonian method given the current [guess]. **)
 let babylonian_step : float -> float -> float =
   fun n guess ->
-    failwith "TODO"
+    (* failwith "TODO" *)
+   guess +. ((n -. square guess) /. (2. *. guess))
 
 (* Task 2.2 *)
 
 (** [babylonian n guess] is a close enough estimate for the square root of [n]
     produced by the Babylonian method given an initial [guess]. **)
-let babylonian : float -> float -> float =
+let rec babylonian : float -> float -> float =
   fun n guess ->
-    failwith "TODO"
+    (* failwith "TODO" *)
+    let new_guess = babylonian_step n guess in
+      if close guess new_guess then new_guess else babylonian n new_guess
+
 
 (******************************************************************************)
 (* Task 3 *)
@@ -43,12 +47,15 @@ let babylonian : float -> float -> float =
     produced by a step of Newton's method given the current [guess]. **)
 let newton_step : (float -> float) -> (float -> float) -> float -> float =
   fun f f' guess ->
-    failwith "TODO"
+    (* failwith "TODO" *)
+    guess -. (f guess /. f' guess)
 
 (* Task 3.2 *)
 
 (** [newton f f' guess] is a close enough estimate for a root of [f]
     produced by Newton's method given an initial [guess]. **)
-let newton : (float -> float) -> (float -> float) -> float -> float =
+let rec newton : (float -> float) -> (float -> float) -> float -> float =
   fun f f' guess ->
-    failwith "TODO"
+    (* failwith "TODO" *)
+    let new_guess = newton_step f f' guess in
+      if close guess new_guess then new_guess else newton f f' new_guess
